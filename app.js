7fd9ea745ce91574ad9320e10302b189a48e4075
Jeton Lolovic <jetlol79@gmail.com>
2018-03-06 22:01:59 -0500

    working nunjucks integration

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const app = express(); // creates a server(creates an instance of an express application)
 const morgan = require("morgan");
+const nunjucks = require("nunjucks");
+
+const locals = {
+	title: "Some Title",
+	people: [
+		{name: "Gandalf"},
+		{name: "Hermione"}
+	]
+};
+
+// nunjucks.configure("views");
+// nunjucks.render("index.html", locals, function(err, output){
+// 	if(err) return console.error(err);
+// 	console.log(output);
+// });
+
+// boiler-plate
+nunjucks.configure("views", {noCache: true}); //where
+app.set("view engine", "html"); // what file extenstion do out templates have
+app.engine("html", nunjucks.render); //how to render html templates
+//boiler-plate
+
 
 app.use(morgan("dev"));
 
 app.get("/", function(req, res){
-	res.send("You got the root route");
+	res.render("index", locals);
 });
 
 app.get("/news", function(req, res){
